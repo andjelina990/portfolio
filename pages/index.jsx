42eef555c783171a9b59bff7365be0bd79f9916f
Andjelina <astojanovic1900@gmail.com>
2022-05-02 16:33:45 +0200

    Add todo

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,15 +15,14 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-cy="homePageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Halo, my name is Andjelina Stojanovic
             </h1>
             <p className="description">
-              Saya seorang pekerja lepas pengembang web, fotograper, desainer
-              antarmuka pengguna, dan penulis.
+              Heloo, i am from Serbia, I am living in Vienna since 2016.
             </p>
 
             <Link href="/contact">
-              <a className="cta">Kontak Saya</a>
+              <a className="cta">Contact me</a>
             </Link>
           </div>
 
